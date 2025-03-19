6aa9bd78c2c7212ad65b956f1c84586f4f4ef19c
Practicum Student <augenoor@gmail.com>
2025-03-19 20:06:28 +0200

    initial commit

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -2,6 +2,41 @@ const page = require('../../page');
 const helper = require('../../helper')
 
 describe('Create an order', () => {
+    it('should set the addresses', async () => {
+        await browser.url(`/`)
+
+
+        const fromField = await $('#from');
+
+        // wait for 10 seconds an element to not exist on the page
+        // await fromField.waitForExist({timeout: 10000, reverse: true});
+                // wait for 10 seconds an element to be visible on the page
+        	await fromField.waitForDisplayed();
+        // {timeout: 10000}wait for 10 seconds an element to be clickable on the page
+        // await fromField.waitForClickable({timeout: 10000});
+
+        await fromField.setValue('East 2nd Street, 601');
+
+
+        const toField = await $('#to')
+
+	// wait for 10 seconds an element to not exist on the page
+        // await toField.waitForExist({timeout: 10000, reverse: true});
+                // wait for 10 seconds an element to be visible on the page
+        	await toField.waitForDisplayed();
+        // {timeout: 10000}wait for 10 seconds an element to be clickable on the page
+        // await toField.waitForClickable({timeout: 10000});
+
+        await toField.setValue('1300 1st St')
+
+        await browser.pause(2000);
+        const addressFrom = await $('#from')
+        await expect(addressFrom).toHaveText('East 2nd Street, 601')
+        const addressTo = await $('#to')
+        await expect(addressTo).toHaveText('1300 1st St')
+
+    })
+
     it('should open phone number modal', async () => {
         await browser.url(`/`)
         await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
--- wdio.conf.js
+++ wdio.conf.js
@@ -86,7 +86,7 @@ exports.config = {
     // with `/`, the base url gets prepended, not including the path portion of your baseUrl.
     // If your `url` parameter starts without a scheme or `/` (like `some/path`), the base url
     // gets prepended directly.
-     baseUrl: 'https://cnt-803039b5-0b29-4d7d-a083-759881d12e34.containerhub.tripleten-services.com',
+     baseUrl: 'https://cnt-48e4db95-1f95-4fd9-adc1-84971fe04af8.containerhub.tripleten-services.com',
     //
     // Default timeout for all waitFor* commands.
     waitforTimeout: 10000,
