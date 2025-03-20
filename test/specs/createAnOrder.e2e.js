11c81463ef01bdbf2278f20d1ff31dd9377bdf99
Practicum Student <augenoor@gmail.com>
2025-03-20 08:11:55 +0200

    initial commit

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -5,25 +5,7 @@ describe('Create an order', () => {
     it('should set the addresses', async () => {
         await browser.url(`/`)
 
-
-        const fromField = await $('#from');
-        await fromField.setValue('East 2nd Street, 601');
-        const toField = await $('#to');
-        await toField.setValue('1300 1st St');
-
-        // Test: if the "Call a taxi" button appears, then the addresses are set
-        const callATaxiButton = await $('//button[contains(text(), "Call a taxi")]');
-        await expect(callATaxiButton).toBeDisplayed();
-
-    }),
-    it('should set the addresses', async () => {
-        await browser.url(`/`)
-
-
-        const fromField = await $('#from');
-        await fromField.setValue('East 2nd Street, 601');
-        const toField = await $('#to');
-        await toField.setValue('1300 1st St');
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
         // Test: if the "Call a taxi" button appears, then the addresses are set
         const callATaxiButton = await $('//button[contains(text(), "Call a taxi")]');
