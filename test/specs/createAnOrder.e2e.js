d9f686f12521d8c2367e82adfccdd4d33f6918e7
Practicum Student <augenoor@gmail.com>
2025-03-21 12:11:05 +0200

    initial commit

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -5,13 +5,8 @@ describe('Create an order', () => {
     it('should fill in the phone number', async () => {
 
         await browser.url(`/`);
-        const fromField = $('#from');
-        fromField.setValue('East 2nd Street, 601');
-        const toField = await $('#to');
-        await toField.setValue('1300 1st St');
 
-        const callATaxiButton = await $('//button[contains(text(), "Call a taxi")]');
-        await callATaxiButton.click();
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
 
         // Test: if the "Supportive" option exists (displayed) after clicking the "Call a taxi" button
         const supportiveOption = await $('//div[contains(text(), "Supportive")]');
